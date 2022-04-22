27c09bf02be74373b35e7b73d039fb44d5778e52
rx570rohit <101522761+rx570rohit@users.noreply.github.com>
2022-04-22 21:11:07 +0530

    Add project files.

---
+++ LineComparison/LineCal.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LineComparison
+{
+    internal class LineCal
+    {
+        int x1, x2, y1, y2;
+
+        Double line;
+        public LineCal(int x1, int y1, int x2, int y2)
+        {
+            this.x1 = x1;
+            this.y1 = y1;
+            this.x2 = x2;
+            this.y2 = y2;
+        }
+        public Double linecal()
+        {
+            return Math.Sqrt(Math.Pow((this.x2 - this.x1), 2));
+        }
+
+    }
+}
---
+++ LineComparison/LineCmpMain.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LineComparison
+{
+    internal class LineCmpMain
+    {
+        public static void Main(String[] arg)
+        {
+            int x1 = Convert.ToInt32(Console.ReadLine());
+            int y1 = Convert.ToInt32(Console.ReadLine());
+            int x2 = Convert.ToInt32(Console.ReadLine());
+            int y2 = Convert.ToInt32(Console.ReadLine());
+
+            LineCal Line1 = new LineCal(x1, y1, x2, y2);
+            Console.WriteLine();
+            Console.WriteLine(Line1.linecal());
+        }
+    }
+}
